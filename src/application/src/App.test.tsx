fbcbb11b20598bae1a7fc374ae78a4c8a7430cbc
srikarjosyam <josyam.srikar@gmail.com>
2021-06-12 15:16:10 +0530

    Added the Tests for UI component

--- src/application/src/App.test.tsx
+++ src/application/src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the App component', () => {
   render(<App />);
   const linkElement = screen.getByText(/Url Shortner/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Check that the Save Button exists and click on it with empty url or invalid Url', () => {
+ render(<App />);
+  const button = screen.getByText('Save')
+  expect(button).toBeInTheDocument();
+  fireEvent.click(button)
+});
+
+test('Add Url and Click on the Save button to creat a valid Url', () => {
+   render(<App />);
+   const button = screen.getByText('Save')
+   const textarea = screen.getByPlaceholderText('Add URL')
+   expect(textarea).toBeInTheDocument();
+   fireEvent.change(textarea, { target: { value: 'https://stackoverflow.com/' } })
+   expect(button).toBeInTheDocument();
+   fireEvent.click(button)
+ });
